6b8964bb6f8e7c5a662abaa613943294dbd7b4ab
bispobr <156684382+bispobr@users.noreply.github.com>
2025-12-13 19:15:46 -0300

    feat:  add enpoints crud

--- src/main/java/spring/gps/controller/PontosInteresseController.java
+++ src/main/java/spring/gps/controller/PontosInteresseController.java
@@ -6,16 +6,15 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import spring.gps.dto.PontosAtualizacaoDTO;
-import spring.gps.dto.PontosInteresseDTO;
-import spring.gps.model.PontosInteresse;
+import spring.gps.dto.PontosRespostaDTO;
+import spring.gps.dto.PontosRequisicaoDTO;
 import spring.gps.repository.PontosInteresseRepository;
 import spring.gps.service.PontosProximoService;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -27,72 +26,48 @@ public class PontosInteresseController {
     @Autowired
     private PontosProximoService pontosProximoService;
 
-    @PostMapping
+    @PostMapping("/cadastro")
     @Operation(description = "Endpoint responsável por cadastrar novos Pontos de interesse")
-    @ApiResponse(responseCode = "200", description = "Transação criada com sucesso")
+    @ApiResponse(responseCode = "201", description = "Novo Ponto de interesse  criada com sucesso")
     @ApiResponse(responseCode = "400", description = "Erro de Requisição")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public ResponseEntity CriarPontosInteresse( @Valid @RequestBody PontosInteresseDTO pontos){
-        pontosInteresseRepository.save(new PontosInteresse(pontos));
-        log.info("Requisição recebida ponto de interesse " + pontos.nome());
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Void> CriarPontosInteresse( @Valid @RequestBody PontosRequisicaoDTO pontos){
+        log.info("Requisição para criação de ponto de interesse recebida ");
+        pontosProximoService.Salvar(pontos);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PutMapping
+    @PutMapping("/{id}")
     @Operation(description = "Endpoint responsável por atualizar Ponto de Interesse")
     @ApiResponse(responseCode = "200", description = "POI atualizado com sucesso")
     @ApiResponse(responseCode = "400", description = "Erro de Requisição, dados enviados não atendem os requisitos")
     @ApiResponse(responseCode = "404", description = "POI não encontrado")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public ResponseEntity atualizarPonto (@Valid @RequestBody PontosAtualizacaoDTO pontos ){
-        log.info("Solicitação de Atualização de POI recebida");
-        Optional<PontosInteresse> optionalPontos = pontosInteresseRepository.findById(pontos.id());
-         if (optionalPontos.isPresent()){
-             PontosInteresse pontoEncontrado = optionalPontos.get();
-             pontoEncontrado.setNome(pontos.nome()); ;
-             pontoEncontrado.setX(pontos.x());
-             pontoEncontrado.setY(pontos.y());
-
-             PontosInteresse pontoAtualizado = pontosInteresseRepository.save(pontoEncontrado);
-             log.info("POI: " + pontoAtualizado.getNome()+ "atualizado com sucesso");
-
-             return ResponseEntity.ok(pontoAtualizado);
-         } else {
-             log.info("POI Não encontrado na base de dados");
-             return ResponseEntity.notFound().build();
-         }
+    public ResponseEntity<PontosRespostaDTO> atualizarPonto (@PathVariable("id")Long id, @Valid @RequestBody PontosRequisicaoDTO pontos ){
+        log.info("Requisição para atualizar  ponto de interesse recebida ");
+        return pontosProximoService.atualizarPonto(id, pontos);
     }
 
-    @DeleteMapping ("/remover/{id}")
+    @DeleteMapping ("/{id}")
     @Transactional
     @Operation(description = "Endpoint responsável por remover POI")
     @ApiResponse(responseCode = "200", description = "POI removido com sucesso")
-    @ApiResponse(responseCode = "400", description = "Erro de Requisição, dados enviados não atendem os requisitos")
     @ApiResponse(responseCode = "404", description = "POI não encontrado")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public ResponseEntity removerPonto ( @PathVariable Long id){
+    public ResponseEntity<Void> removerPonto ( @PathVariable Long id){
         log.info("Solicitação de remoção de POI recebida");
-        Optional<PontosInteresse> optionalPontos = pontosInteresseRepository.findById(id);
-        if (optionalPontos.isPresent()){
-            pontosInteresseRepository.deleteById(id);
-            log.info("POI removido com sucesso");
-            return ResponseEntity.ok().build();
-        } else {
-            log.info("POI Não encontrado na base de dados");
-            return ResponseEntity.notFound().build();
-        }
+        return pontosProximoService.removerPonto(id);
     }
 
 
-    @GetMapping
+    @GetMapping("/listagem")
     @Operation(description = "Endpoint responsável por retornar todos os Pontos de interesse")
     @ApiResponse(responseCode = "200", description = "Lista retornada com sucesso")
     @ApiResponse(responseCode = "400", description = "Erro de Requisição")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public  ResponseEntity<List<PontosInteresse>> retornarPontos (){
-        List<PontosInteresse>listaPontosInteresse = pontosInteresseRepository.findAll();
-        log.info("Listando todos os pontos de interesse" );
-        return ResponseEntity.ok(listaPontosInteresse);
+    public  ResponseEntity<List<PontosRespostaDTO>> retornarPontos (){
+        log.info("Solicitação de Listagem  de POIs recebida");
+        return ResponseEntity.ok(pontosProximoService.retornarListaPontos());
     }
 
     @GetMapping("/proximos")
@@ -100,9 +75,8 @@ public class PontosInteresseController {
     @ApiResponse(responseCode = "200", description = "Lista interesse proximo retornada com sucesso")
     @ApiResponse(responseCode = "400", description = "Erro de Requisição")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public  ResponseEntity<List<PontosInteresse>> retornarPontosProximos (@RequestParam("x")Long x,@RequestParam("y")Long y,@RequestParam(value ="dmax", required = false,defaultValue = "10")Long dmax){
+    public  ResponseEntity<List<PontosRespostaDTO>> retornarPontosProximos (@RequestParam("x")Long x, @RequestParam("y")Long y, @RequestParam(value ="dmax", required = false,defaultValue = "10")Long dmax){
         log.info("Requsição de listagem de  todos os pontos de interesse proximos recebida" );
-        List<PontosInteresse>  filtradosPontos = pontosProximoService.pontosProximos(x,y,dmax);
-        return ResponseEntity.ok(filtradosPontos);
+        return ResponseEntity.ok(pontosProximoService.pontosProximos(x,y,dmax));
     }
 }
